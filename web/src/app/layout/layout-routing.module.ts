8f50d4ba6ab9074bcb01e8268d16ed5491e70fc4
rattapol <bible6666@hotmail.com>
2018-05-15 22:11:08 +0700

    Add page company

--- web/src/app/layout/layout-routing.module.ts
+++ web/src/app/layout/layout-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
         children: [
             { path: '', redirectTo: 'dashboard' },
             { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
+            { path: 'master/company', loadChildren: './master/company/company.module#CompanyModule' },
             { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
             { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
             { path: 'forms', loadChildren: './form/form.module#FormModule' },
