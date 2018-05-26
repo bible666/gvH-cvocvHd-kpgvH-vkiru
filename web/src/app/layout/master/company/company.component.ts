f822d117a1a1247a62fd278282315339a37bf184
rattapol <bible6666@hotmail.com>
2018-05-26 22:12:12 +0700

    update component company

--- web/src/app/layout/master/company/company.component.ts
+++ web/src/app/layout/master/company/company.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../../router.animations';
 
 @Component({
     selector: 'app-blank-page',
     templateUrl: './company.component.html',
-    styleUrls: ['./company.component.scss']
+    styleUrls: ['./company.component.scss'],
+    animations: [routerTransition()]
 })
 export class CompanyComponent implements OnInit {
     constructor() {}
