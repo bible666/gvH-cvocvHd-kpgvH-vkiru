4cff947e67e7c5d4b40e2fb8932fe68ac4686200
rattapol <bible6666@hotmail.com>
2018-06-15 23:14:33 +0700

    use class for show data in html

--- web/src/app/layout/master/company/company.component.ts
+++ web/src/app/layout/master/company/company.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../../router.animations';
 
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap'
+
 @Component({
     selector: 'app-blank-page',
     templateUrl: './company.component.html',
@@ -9,16 +11,30 @@ import { routerTransition } from '../../../router.animations';
 })
 export class CompanyComponent implements OnInit {
     
-    DataForGrid : string;
+    DataForGrid : company[];
     jsonObj : object;
 
     AllPages : number[];
     CurrentPage : number;
 
-    constructor() {
+    constructor(
+        public modalService : NgbModal
+    ) {
 
-        this.DataForGrid = '{"datas":[{"companyId":1,"companyName":"Com1"},{"companyId":2,"companyName":"Com2"}]}';
-        this.jsonObj  = JSON.parse(this.DataForGrid);
+        this.DataForGrid = [];
+        let one : company = new company();
+        one.id = 1;
+        one.company_name = "Company 1";
+
+        this.DataForGrid.push(one);
+
+        let two : company = new company();
+        two.id = 2;
+        two.company_name = "Company 2";
+
+        this.DataForGrid.push(two);
+        //this.DataForGrid = '{"datas":[{"companyId":1,"companyName":"Com1"},{"companyId":2,"companyName":"Com2"}]}';
+        //this.jsonObj  = JSON.parse(this.DataForGrid);
 
         this.AllPages = [];
         for (let i = 0 ; i <= 10 ; i++){
@@ -29,4 +45,16 @@ export class CompanyComponent implements OnInit {
     }
 
     ngOnInit() {}
+}
+
+export class company{
+    id : number;
+    company_name : string;
+    company_shot_name : string;
+    address : string;
+    contact_name : string;
+    contact_phone : string;
+    contact_email : string;
+    register_date;
+    status : number;
 }
--- web/src/app/layout/master/company/company.module.ts
+++ web/src/app/layout/master/company/company.module.ts
@@ -4,8 +4,15 @@ import { CommonModule } from '@angular/common';
 import { CompanyRoutingModule } from './company-routing.module';
 import { CompanyComponent } from './company.component';
 
+import { NgbModule, NgbDropdownModule, NgbModalModule } from '@ng-bootstrap/ng-bootstrap'
 @NgModule({
-    imports: [CommonModule, CompanyRoutingModule],
+    imports: [
+        CommonModule, 
+        CompanyRoutingModule,
+        NgbModule.forRoot(),
+        NgbDropdownModule,
+        NgbModalModule
+    ],
     declarations: [CompanyComponent]
 })
 export class CompanyModule {}
