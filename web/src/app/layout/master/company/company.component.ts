8294b727a0039cf4235dd3fe3a67e0fda84f9e2c
rattapol <bible6666@hotmail.com>
2018-06-15 22:56:48 +0700

    Add Max and current page

--- web/src/app/layout/master/company/company.component.ts
+++ web/src/app/layout/master/company/company.component.ts
@@ -8,7 +8,25 @@ import { routerTransition } from '../../../router.animations';
     animations: [routerTransition()]
 })
 export class CompanyComponent implements OnInit {
-    constructor() {}
+    
+    DataForGrid : string;
+    jsonObj : object;
+
+    AllPages : number[];
+    CurrentPage : number;
+
+    constructor() {
+
+        this.DataForGrid = '{"datas":[{"companyId":1,"companyName":"Com1"},{"companyId":2,"companyName":"Com2"}]}';
+        this.jsonObj  = JSON.parse(this.DataForGrid);
+
+        this.AllPages = [];
+        for (let i = 0 ; i <= 10 ; i++){
+            this.AllPages.push(i);
+        }
+        this.CurrentPage = 2;
+        console.log(this.jsonObj);
+    }
 
     ngOnInit() {}
 }
--- web/src/app/layout/master/company/company.module.ts
+++ web/src/app/layout/master/company/company.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { CompanyRoutingModule } from './company-routing.module';
 import { CompanyComponent } from './company.component';
-import { MyGridComponent } from '../../components/my-grid/my-grid.component';
+
 @NgModule({
     imports: [CommonModule, CompanyRoutingModule],
-    declarations: [CompanyComponent,MyGridComponent]
+    declarations: [CompanyComponent]
 })
 export class CompanyModule {}
